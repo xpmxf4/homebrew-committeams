9fcf24ab1e737222ce1d06f671961b1d60a4f6f9
dev-chpark <xpmxf5@gmail.com>
2024-07-21 17:00:43 +0900

    🐞 fix: username 수정

--- Formula/commitTeams.rb
+++ Formula/commitTeams.rb
@@ -1,6 +1,6 @@
 class CommitTeams < Formula
   desc "Interactive Git commit message generator for teams with custom templates"
-  homepage "https://github.com/<your-username>/homebrew-committeams"
+  homepage "https://github.com/xpmxf4/homebrew-committeams"
   url "https://github.com/<your-username>/homebrew-committeams/raw/main/commitTeams.sh"
   version "1.0.0"
   sha256 "91e0bd1c22b070f0c9fa5189b2a9b82fa59d46fecf174a43256a0ad83eb3e0fd"
